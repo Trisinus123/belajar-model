9ccb7709b20dc3878806a174532290a4684ebde4
Trisinus123 <trisinusgl@gmail.com>
2023-03-14 23:17:48 +0700

    praktikum empat

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -16,7 +16,7 @@ class PostSeeder extends Seeder
     {
         DB::table('posts')->insert([
             'title' => 'Belajar Model Dengan Laravel',
-            'slug' => ' belajar-model-dengan-laravel',
+            'slug' => 'belajar-model-dengan-laravel',
             'image' => 'https://via.placeholder.com/640x480.png/00ff77?text=Belajar Model',
             'content' => 'Belajar dengan Laravel sungguh menyenangkan',
             'draft' => 0
